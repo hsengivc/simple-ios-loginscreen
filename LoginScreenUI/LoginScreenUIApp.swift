d4353d9af9cab658416b4070eacba26544764805
hsengivc <hsengivc@proton.me>
2023-04-05 20:34:47 +0530

    Initial Commit

---
+++ LoginScreenUI/LoginScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenUIApp.swift
+//  LoginScreenUI
+//
+//  Created by Vignesh Chinnusamy on 05/04/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
