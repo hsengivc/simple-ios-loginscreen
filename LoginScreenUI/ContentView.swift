3f4b92576dabb8a55b3e46f744ee05b5f3f51854
hsengivc <hsengivc@proton.me>
2023-04-05 21:25:44 +0530

    login ui code

--- LoginScreenUI/ContentView.swift
+++ LoginScreenUI/ContentView.swift
@@ -1,21 +1,69 @@
-//
-//  ContentView.swift
-//  LoginScreenUI
-//
-//  Created by Vignesh Chinnusamy on 05/04/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var wrongEmail = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                Color.blue
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+                VStack {
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Email", text: $email)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongEmail))
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    Button("Login") {
+                        authenticateUser(email: email, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("You are logged in \(email)"), isActive: $showingLoginScreen) {
+                        EmptyView()
+                    }
+                }
+            }
+            .navigationBarHidden(true)
         }
-        .padding()
+    }
+    
+    func authenticateUser(email: String, password: String) {
+        if email.lowercased() == "test@gmail.com" {
+            wrongEmail = 0
+            if password.lowercased() == "test123" {
+                wrongPassword = 0
+                showingLoginScreen = true
+            } else {
+                wrongPassword = 2
+            }
+        } else {
+            wrongEmail = 2
+        }
     }
 }
 
